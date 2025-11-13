235745f59a3177603a3c96498d68a3a5ad7f196c
SarkerMdTalha <smsakib62@gmail.com>
2025-11-13 12:45:20 +0600

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-
+import './assets/css/style.css'
+import Header from './components/Header.jsx'
+import Footer from './components/footer.jsx'
+import Main from './components/Main.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+    return (
+        <>
+            <div className='container'>
+                <div className='p-5 text-center bg-light-dark rounded'>
+                    <h1 className='text-light'>Stock Prediction Portal</h1>
+                    <p className='text-light lead'>This is your stock prediction portal. Predict the stocks and be a stock market expert through our site. Thank you for using our site</p>
+                    <Button text='login' class='btn-outline-info' />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main

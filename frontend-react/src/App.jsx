58347cab097d66c0c13f68ec4fdafff6b042e579
SarkerMdTalha <smsakib62@gmail.com>
2025-11-14 22:38:51 +0600

    Registration Page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 import './assets/css/style.css'
+import Main from './components/Main.jsx'
+import Register from './components/Register.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Footer from './components/footer.jsx'
-import Main from './components/Main.jsx'
+import Login from './components/login.jsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter >
+
     </>
   )
 }
---
+++ frontend-react/src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+    return (
+        <>
+            <div className='text-light container'>Login</div>
+        </>
+    )
+}
+
+export default login
